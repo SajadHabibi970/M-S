8849751fe3cc7c9085ae653464ad79fed0755847
Sajad Habibi <sajad.habibi@edu.newton.se>
2026-01-08 14:09:49 +0100

    Initial commit

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Console.WriteLine("Welcome to Quiz battle");
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Quiz battle regler");
+
+Console.WriteLine("Svara på frågor korrekt för att tjäna poäng");
+
+while (!QuizUtils.IsCompleted())
+{
+    QuizUtils.DisplayQuestion();
+    int answer = QuizUtils.GetAnswer();
+    QuizUtils.CheckAnswer(answer);
+
+    QuizUtils.WriteStatus();
+}
+Console.WriteLine("Tack för att du spelade");
